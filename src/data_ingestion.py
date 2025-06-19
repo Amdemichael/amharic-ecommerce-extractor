26785d167f81caf87caddf8e97041f5d16c2f07e
Amdemichael Kidane <amdek2003@gmail.com>
2025-06-19 13:30:48 +0300

    fix

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -66,11 +66,11 @@ async def main():
 
 if __name__ == "__main__":
     nest_asyncio.apply()
-    
+
     try:
         loop = asyncio.get_event_loop()
     except RuntimeError:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
-    
+
     loop.run_until_complete(main())
